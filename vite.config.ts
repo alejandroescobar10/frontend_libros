418de6d919df68a691ca9b758c9260faaa778eae
alejandroescobar10 <160554099+alejandroescobar10@users.noreply.github.com>
2025-05-29 23:39:02 -0500

    fronten 6

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist' // Vercel detecta esta carpeta por defecto
+  }
 })
